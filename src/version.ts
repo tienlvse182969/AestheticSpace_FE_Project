615a29bf657e13113d038765d3d871ed7f1026ee
Le Van Tien <tienlvse182969@fpt.edu.vn>
2026-08-05 23:42:23 +0700

    chore: removed beta badge

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = "1.0.9 Beta";
+export const APP_VERSION = "1.0.9";
 export const RELEASE_NOTES_URL = "https://aestheticspace-releasenote.notion.site/Ghi-ch-ph-t-h-nh-3a5c68c3bea2804abc88e879a1276f7f?source=copy_link";
